32422e9cb0a3c9081d81d0c22748eb1d219ca68e
zhangxv <xuzhang@thoughtworks.com>
2015-03-17 21:09:49 +0530

    a triangle must have width

---
+++ src/Triangle.java
@@ -0,0 +1,11 @@
+public class Triangle {
+    private final int width;
+
+    public Triangle(int width) {
+        this.width = width;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+}
